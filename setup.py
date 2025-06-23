1e4bdd720a17a41a105b5991b716ada130a12ab0
Stuart Lynne <stuart.lynne@gmail.com>
2025-06-23 15:28:43 -0700

    setup for install, version

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Dynamically load version
+version_ns = {}
+with open("version.py") as f:
+    exec(f.read(), version_ns)
+
+setup(
+    name="solar12vups",
+    version=version_ns["__version__"],
+    description="Solar-powered UPS monitor with BLE support and real-time GUI",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(include=["app*", "ble*", "lib*", "gui*"]),
+    py_modules=["solar_runner"],
+    install_requires=[
+        "tabulate"
+    ],
+    entry_points={
+        "console_scripts": [
+            "solar12vups=solar_runner:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
